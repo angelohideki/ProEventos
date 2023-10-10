500bc9838b5fb098fae4469ec6cb4732333fb452
Ângelo Hideki Noda <angelonoda@gmail.com>
2023-10-10 08:29:35 -0300

    Front

--- Back/ProEventos.API/Controllers/EventoController.cs
+++ Back/ProEventos.API/Controllers/EventoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
 using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers;
@@ -7,40 +8,25 @@ namespace ProEventos.API.Controllers;
 [Route("api/[controller]")]
 public class EventoController : ControllerBase
 {
-    public IEnumerable<Evento> _evento = new Evento[] {
-         new Evento() {
-             EventoId = 1,
-            Tema = "Angular 11 e .Net 6",
-            Local = "Curitiba",
-            Lote = "1º Lote",
-            QtdPessoas = 250,
-            DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-            ImagemUrl = "foto.png"
-           },
-           new Evento() {
-             EventoId = 2,
-            Tema = "Angular 11 e .Net 6",
-            Local = "São Paulo",
-            Lote = "2º Lote",
-            QtdPessoas = 350,
-            DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-            ImagemUrl = "foto.png"
-           }
-    };
-    public EventoController()
-    {        
+    private readonly DataContext _context;
+
+    public EventoController(DataContext context)
+    {
+        _context = context;
     }
 
     [HttpGet]
     public IEnumerable<Evento> Get()
     {    
-        return _evento;
+        return _context.Eventos;
     }
 
     [HttpGet("{id}")]
-    public IEnumerable<Evento> GetById(int id)
+    public Evento GetById(int id)
     {    
-        return _evento.Where(evento => evento.EventoId == id);
+        return _context.Eventos.FirstOrDefault(
+            evento => evento.EventoId == id
+           );
     }
 
     [HttpPost]
--- Back/ProEventos.API/Program.cs
+++ Back/ProEventos.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventos.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+#pragma warning disable CS8600 // Conversão de literal nula ou possível valor nulo em tipo não anulável.
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+#pragma warning restore CS8600 // Conversão de literal nula ou possível valor nulo em tipo não anulável.
+
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseMySql(mySqlConnection, ServerVersion.AutoDetect(mySqlConnection)));
+
 
 var app = builder.Build();
 

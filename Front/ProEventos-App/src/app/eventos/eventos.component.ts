fa508f5bfbe6fcc79fda01ffd13eb84166118a17
Ângelo Hideki Noda <angelonoda@gmail.com>
2023-10-10 22:56:00 -0300

    Adicionado Bootstrap e Fontawesome free

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class EventosComponent {
 
+  public eventos: any;
+  constructor(private http: HttpClient){ }
+
+  ngOnInit(): void{
+    this.getEventos();
+  }
+
+  public getEventos(): void{
+    this.http.get('https://localhost:7026/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+    );
+  }
 }
